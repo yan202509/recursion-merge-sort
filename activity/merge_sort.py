bc5c386d53e27b17d5b4297afb0c1fa2d09f3561
YanYi Chen <vickyeah13@gmail.com>
2025-11-06 11:39:08 -0800

    wrote recursion function

--- activity/merge_sort.py
+++ activity/merge_sort.py
@@ -6,4 +6,14 @@ from .mergelib import merge_sorted
 # with respect to the combined lengths of the lists.
 
 def merge_sort(data):
-    pass
+    # base
+    if len(data) <= 1: 
+        return list(data)
+
+    mid = len(data)//2
+
+    left = merge_sort(data[:mid])
+    right = merge_sort(data[mid:])
+
+
+    return merge_sorted(left, right)
